e7e1735a7fd986bd6c593edc94f8eae51f31b499
Maady <maadhesh.off@gmail.com>
2026-01-11 17:44:58 +0530

    some work

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = {'docx', 'pdf'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.get("/")
 def main():
-    print("Hello from jd-to-resume!")
-    return "Hello from jd-to-resume!" ,200
+    return render_template("home.html") ,200
 
-@app.post("/details")
+@app.post("/generate_resume")
 def details():
-    return ""
+    file=request.files["resume_file"]
+    job_description=request.form["job_description"]
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+            # Process the file and job description here
+            return render_template("result.html", job_description=job_description), 200
+    flash('Invalid file type')
+    return redirect(request.url)
 
-@app.get("/progress/<id:int>")
-def progress(id):
-    return ""
 
-@app.post('/download/<id:int>')
-def download(id):
-    return ""
 
 
 if __name__ == "__main__":
